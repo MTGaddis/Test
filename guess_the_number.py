b6dba55fb5e6060ef750c5c7f774595ea4c29d31
Michael Gaddis <mgaddis88@gmail.com>
2023-07-15 04:34:27 -0400

    adding multiplayer

--- guess_the_number.py
+++ guess_the_number.py
@@ -21,52 +21,60 @@ def guess_the_number():
     min_range, max_range = difficulty_levels[selected_difficulty]
     random_number = random.randint(min_range, max_range)
     max_attempts = 7  # Set the maximum number of attempts
-    attempts = 0
 
-    while attempts < max_attempts:
-        guess = input(f"Guess the number (between {min_range} and {max_range}): ")
+    num_players = int(input("Enter the number of players: "))
+    player_scores = {}  # Dictionary to store player scores
 
-        # Validate user input
-        if not guess.isdigit():
-            print("Invalid input. Please enter a numeric value.")
-            continue
+    for player in range(1, num_players + 1):
+        player_name = input(f"Enter the name of Player {player}: ")
+        attempts = 0
 
-        guess = int(guess)
-        if guess < min_range or guess > max_range:
-            print(f"Out of range. Please enter a number between {min_range} and {max_range}.")
-            continue
+        while attempts < max_attempts:
+            guess = input(f"{player_name}, guess the number (between {min_range} and {max_range}): ")
 
-        attempts += 1
+            # Validate user input
+            if not guess.isdigit():
+                print("Invalid input. Please enter a numeric value.")
+                continue
 
-        if guess == random_number:
-            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            guess = int(guess)
+            if guess < min_range or guess > max_range:
+                print(f"Out of range. Please enter a number between {min_range} and {max_range}.")
+                continue
 
-            # Update high scores
-            update_high_scores(selected_difficulty, attempts, high_scores)
+            attempts += 1
 
-            break
-        elif guess < random_number:
-            print("Too low! Guess a higher number.")
-        else:
-            print("Too high! Guess a lower number.")
+            if guess == random_number:
+                print(f"Congratulations, {player_name}! You guessed the number in {attempts} attempts.")
 
-        remaining_attempts = max_attempts - attempts
-        print(f"You have {remaining_attempts} attempts remaining.")
+                # Update player scores
+                if player_name not in player_scores or attempts < player_scores[player_name]:
+                    player_scores[player_name] = attempts
 
-    if attempts >= max_attempts:
-        print("Game over! You have reached the maximum number of attempts.")
+                break
+            elif guess < random_number:
+                print("Too low! Guess a higher number.")
+            else:
+                print("Too high! Guess a lower number.")
 
-    # Display high scores
-    show_high_scores(high_scores)
+            remaining_attempts = max_attempts - attempts
+            print(f"{player_name}, you have {remaining_attempts} attempts remaining.")
+
+        if attempts >= max_attempts:
+            print(f"{player_name}, game over! You have reached the maximum number of attempts.")
+
+    # Display player scores
+    show_player_scores(player_scores)
+
+    # Update and save high scores
+    update_high_scores(selected_difficulty, player_scores, high_scores)
+    save_high_scores(high_scores)
 
     play_again = input("Do you want to play again? (yes/no): ")
     if play_again.lower() == "yes":
         guess_the_number()
     else:
         print("Thank you for playing!")
-
-    # Save high scores to a file
-    save_high_scores(high_scores)
 
 def load_high_scores():
     try:
@@ -77,9 +85,9 @@ def load_high_scores():
         high_scores = {}
     return high_scores
 
-def update_high_scores(difficulty, attempts, high_scores):
-    if difficulty not in high_scores or attempts < high_scores[difficulty]:
-        high_scores[difficulty] = attempts
+def update_high_scores(difficulty, player_scores, high_scores):
+    if difficulty not in high_scores or sum(player_scores.values()) < high_scores[difficulty]:
+        high_scores[difficulty] = sum(player_scores.values())
 
 def save_high_scores(high_scores):
     with open("high_scores.csv", mode="w", newline="") as file:
@@ -88,10 +96,10 @@ def save_high_scores(high_scores):
         for difficulty, attempts in high_scores.items():
             writer.writerow([difficulty, attempts])
 
-def show_high_scores(high_scores):
-    print("High Scores:")
-    print("Difficulty\tAttempts")
-    for difficulty, attempts in high_scores.items():
-        print(f"{difficulty}\t\t{attempts}")
+def show_player_scores(player_scores):
+    print("Player Scores:")
+    print("Player\tAttempts")
+    for player, attempts in player_scores.items():
+        print(f"{player}\t{attempts}")
 
 guess_the_number()
